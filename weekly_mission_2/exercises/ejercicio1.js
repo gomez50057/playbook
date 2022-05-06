83a6c685fa423260e565a54caf9e8fbcf9747d0a
gomez50057 <37481091+gomez50057@users.noreply.github.com>
2022-05-05 19:28:50 -0500

    1 y 2

---
+++ weekly_mission_2/exercises/ejercicio1.js
@@ -0,0 +1,103 @@
+const repo = {
+    name: "LaunchX",
+    author: "carlogilmar",
+    language: "JavaScript",
+    numberOfCommits: 100,
+    stars: 199,
+    forks: 299,
+    issues_open: 10,
+    issues_close: 10,
+    getTotalIssues: function(){
+      return this.issues_open + this.issues_close
+    },
+    getGeneralInfo: function(){
+      return 
+    }
+}
+
+const Issue={
+    title: "Issue",
+    repositoryNameAssociated: "LaunchX",
+    status: "Open",
+    numberOfComments: 4,
+    labels: "bug",
+    author: "Gomez50057",
+    dateCreated: "05/05/2022",
+    lastUpdated: "05/05/2022",
+    getTitleAndAuthor(){
+        return `title: ${this.title}  author: ${this.author}`
+    },
+    getGeneralInfo(){
+        return `Issue ${this.name} fue creado por ${this.author} bajo el label de ${this.labels}`
+    }
+}
+   
+const PullRequest ={
+    title: "Ejercicio",
+    branchName: "master",
+    dateCreated: "05/05/2022",
+    status: "En revisión",
+    repositoryNameAssociated: "LauncXCopia",
+    getStatus(){
+        return `El pull_request ${this.title} se encuentra ${this.status}`
+    },
+    getTitleAndAuthor(){
+        return `title: ${this.title}  author: ${this.author}`
+    }
+}
+
+
+//Twitter
+
+const TwitterUser={
+    user: "@gomez50057",
+    username: "@gomez50057",
+    bio: "Vivimos en un arcoíris de caos",
+    dateCreated: "02/09/2022",
+    followers: 50,
+    following: 50,
+}
+
+const TwitterHashTag={
+    Theme:["Para ti","Tendencias","COVID-19","Noticias","Entretenimiento"]
+}
+
+//Facebook
+const FacebookUser={
+    user: "Gabriel Gomez",
+    friends: 628,
+    theme:["Publicaciones","Información","Amigos","Fotos","Videos","Registros","Más"],
+}
+const FacebookPost={
+    user: "Gabriel Gomez",
+    dateCreated: "Ayer",
+    message: "Pase de abordar",
+    picture: "foto pase",
+    reactions:{
+        like: 15,
+        corazon: 20,
+        divertido: 0,
+        triste: 0,
+        enojado: 0
+    }
+}
+const FacebookBiography={
+    friends:["Todos los amigos","Cumpleaños","Ciudad actual","Ciudad de origen","Seguidores","Seguidos"],
+    fotos:["Fotos en las que apareces","Tus fotos","Álbumes"],
+}
+
+//Uber
+const UberProfile={
+    name: "Gabriel Gomez",
+    puntuacion: 4.5,
+    cajas:{
+        Ayuda:["Ayuda con un viaje","Cuenta y opciones de pago","Más","Tu guía Uber","Uber Rewards","Regístrate","Accesibilidad","Emergencia","Uber Moto","información sobre el pase"],
+        Pago: ["Efectivo","Agregar método de pago","Cupones","Agregar código del cupón","Agregar un código de promo"],
+    },
+    Mensajes:[],
+    Configuracion:[],
+    Legal:[]
+} 
+console.log(UberProfile);
+console.log(TwitterUser);
+console.log(FacebookUser);
---
+++ weekly_mission_2/exercises/ejercicio2.js
@@ -0,0 +1,86 @@
+const explorers = [
+  {
+    name: "Explorer 1",
+    exercises_completed: 10,
+    rate: 99,
+    city: "CDMX",
+    stack: [
+      "js",
+      "c#"
+    ],
+    missions: {
+      onboarding: {
+        isFinished: true,
+        exercisesFinished: true
+      },
+      frontend: {
+        isFinished: true,
+        exercisesFinished: true
+      }
+    }
+  },
+  {
+    name: "Explorer 2",
+    exercises_completed: 9,
+    city: "Veracruz",
+    rate: 50,
+    stack: [
+      "js"
+    ],
+    missions: {
+      onboarding: {
+        isFinished: false,
+        exercisesFinished: false
+      },
+      frontend: {
+        isFinished: false,
+        exercisesFinished: false
+      }
+    }
+  },
+  {
+    name: "Explorer 3",
+    exercises_completed: 3,
+    city: "Sonora",
+    rate: 100,
+    stack: [
+      "elixir"
+    ],
+    missions: {
+      onboarding: {
+        isFinished: true,
+        exercisesFinished: true
+      },
+      frontend: {
+        isFinished: false,
+        exercisesFinished: false
+      }
+    }
+  }
+]
+   console.log("--------------------------------------")
+   explorers.forEach(nombre => console.log('Nombre Explorer: '+ nombre.name +' stack: '+ nombre.stack));
+   
+   console.log("--------------------------------------")
+   const listastack = explorers.map((sta) => sta.stack);
+   console.log(listastack);
+   
+   console.log("---------------------------------------")
+   const listajs = explorers.filter((lis) => lis.stack.includes('js'))
+   console.log(listajs)
+
+   console.log("---------------------------------------")
+   const queseadecdmx = explorers.find((cdmx) => cdmx.city === 'CDMX');
+  console.log("El que vive en la CDMX es el: " + queseadecdmx.name);
+
+  console.log("Suma de  de ejercicios completos usando reduce")
+  const sumaejercicioscompletos = explorers.reduce ((acumulador, valoractual) =>  acumulador + valoractual.exercises_completed, 0)
+  console.log(sumaejercicioscompletos);
+
+
+  const siestrueconsome = explorers.some((t)=> t.exercisesFinished || true)
+  console.log("es:"  + siestrueconsome);
+
+
+  const siestrueconevery = explorers.every((name) => name.isFinished || true)
+  console.log("es:" + siestrueconevery);
